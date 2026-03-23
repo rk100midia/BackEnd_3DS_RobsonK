498ae9c69f0af30c45927db3e4b84d170ce33b56
rk100midia <robson.cropolato@escola.pr.gov.br>
2026-03-23 12:14:14 +0000

    finalização de objetos js

---
+++ objetos/obj-01.js
@@ -0,0 +1,9 @@
+const carros ={
+    nome: "camaro",
+    modelo: "xpto Cam",
+    cores: ["Amarelo", "preto"]
+}
+carros.modelo = "fusca"
+carros["nome"] = "Chev"
+console.log(carros.modelo)
+console.log(carros.nome)
---
+++ objetos/obj-02.js
@@ -0,0 +1,19 @@
+const time = {
+    estádio: "ligga Arena",
+    torcida: "Os fanáticos",
+    comandoTécnico: "Odair Hellmann",
+    títulos:"35 títulos oficiais",
+    Artilheiro: "Barcímio sicupira Júnior"
+}
+time.estádio = "Ligga arena"
+time.torcida = "Os fanáticos"
+time.comandoTécnico = "Odair Hellmann"
+time.títulos = "35 títulos oficiais"
+time.Artilheiro = "Barcímio sicupira júnior"
+
+console.log(time.estádio)
+console.log(time.torcida)
+console.log(time.comandoTécnico)
+console.log(time.títulos)
+console.log(time.Artilheiro)
+console.log("ligga Arena:" + time.estádio)
