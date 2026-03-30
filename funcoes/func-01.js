74f40165cb3e27c954558b7e5eb6dff1699a1b3c
rk100midia <robson.cropolato@escola.pr.gov.br>
2026-03-30 10:59:04 +0000

    finalizamos função

---
+++ funcoes/func-01.js
@@ -0,0 +1,14 @@
+// Funções são blocos de código reaproveitáveis
+// podem ou não receber parâmetros
+// podem ou não retornar valores
+
+function dizOla(){
+    console.log("Olá")
+}
+function olaPessoa(nome){
+    console.log("Olá, " + nome)
+}
+
+dizOla()
+olaPessoa("Luana")
+olaPessoa("sla")
---
+++ funcoes/func-02.js
@@ -0,0 +1,40 @@
+//crie 4 funções para operações (+, -, *, /)
+// calcula e imprime resultado
+
+function subtração(n1, n2){
+    const resultado = n1 - n2
+    console.log("resultado subtração: " + resultado)
+}
+
+subtração(5, 4)
+subtração(4, 2)
+
+//adição
+
+function adição(n1, n2){
+    const resultado = n1 + n2
+    console.log("resultado adição: " + resultado)
+}
+
+adição(15, 10)
+adição(20, 10)
+
+//divisão
+
+function divisão(n1, n2){
+    const resultado = n1 / n2
+    console.log("resultado divisão : " + resultado)
+}
+
+divisão(5, 4)
+divisão(6, 4)
+
+//multiplicação
+
+function multiplicação(n1, n2){
+    const resultado = n1 * n2
+    console.log("resultado multiplicação: " + resultado)
+}
+
+multiplicação(5, 4)
+multiplicação(15, 10)
