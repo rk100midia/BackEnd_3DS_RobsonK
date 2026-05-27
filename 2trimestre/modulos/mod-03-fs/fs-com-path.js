4f87db24dcb525cb0cdc51a7f63587439d27d138
rk100midia <robson.cropolato@escola.pr.gov.br>
2026-05-27 11:48:46 +0000

    Finalização do Módulo FS com Path (join e resolve)

--- 2trimestre/modulos/mod-03-fs/fs-com-path.js
+++ 2trimestre/modulos/mod-03-fs/fs-com-path.js
@@ -1,5 +1,12 @@
-//importar PATH e FS
+// importar PATH e FS
+// Path possui (join para juntar caminhos e resolve
+// para deduzir subdiretórios e S.O.)
+const fs = require ('fs')
+const path = require('path')
 
 //Salvar o Caminho em uma variável
+//const caminho = path.join(__dirname, 'README.md')
+const caminho = path .resolve(__dirname, 'README.md')//Abrir, ler, deletar, editar
+const arquivo = fs.readFileSync (caminho, 'utf-8')
 
-//Abrir, ler, deletar, editar
+console.log(arquivo)
